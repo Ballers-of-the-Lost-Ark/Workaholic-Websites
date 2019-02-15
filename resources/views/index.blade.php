db080c3c8757a0f4b9f62cd8c01647edb742e8ec
Nick Zinn <nzinn43@gmail.com>
2019-02-15 13:39:18 -0700

    second commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,7 @@
 </head>
 <body class="overflow-x">
     <div class="home" id="particles-js">
-        @include('inc.navBar')
+        @include('inc.navBar.blade.php')
         @include('inc.messages')
 
         <div class="row">
