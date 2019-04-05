bea78070334898b4e42919f23cfddb1e26ad2134
Nick Zinn <nzinn43@gmail.com>
2019-04-05 15:13:26 -0600

    adding YVCC

--- resources/js/service-redirect.js
+++ resources/js/service-redirect.js
@@ -1,8 +1,8 @@
 if (window.location.pathname == '/') {
     Array.from(document.querySelectorAll('.card-service')).forEach(function(card){
         card.addEventListener('click', function(){
-            window.location.href = "http://workaholic.test/services";
-           // window.location.href = "https://workaholic-websites.com/services";
+            //window.location.href = "http://workaholic.test/services";
+           window.location.href = "https://workaholic-websites.com/services";
         });
     });
 }
